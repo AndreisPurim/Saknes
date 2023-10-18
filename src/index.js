c0629fe3aacb7ee7749e2de5ec4055a1b64bff75
Andreis Purim <andreispurim@gmail.com>
2023-10-17 22:33:16 -0300

    Restarting project, removed d3tree

--- src/index.js
+++ src/index.js
@@ -1,27 +1,105 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import { create } from 'pinch-zoom-pan';
+import css from './tree.module.css';
+import ReactFamilyTree from 'react-family-tree';
+import './index.css'
 
-import Tree from './FamilyTree/Tree.js';
-import Home from './Home/Home.js';
-import Map from './Map/Map.js';
+import classNames from 'classnames';
 
-var Families_List = ['Purim','Klavin'].sort()
+export const PinchZoomPan = React.memo(
+  function PinchZoomPan({ min, max, captureWheel, className, style, children }) {
+    const root = React.useRef(null);
+    React.useEffect(() => {
+      const element = root.current;
+      if (!element) return;
+      return create({ element, minZoom: min, maxZoom: max, captureWheel });
+    }, [min, max, captureWheel]);
+    return (
+      <div ref={root} className={classNames(className, css.root)} style={style}>
+        <div className={css.point}>
+          <div className={css.canvas}>
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+  
+function FamilyNode({ node, isRoot, isHover, onClick, onSubClick, style }){
+    const clickHandler = React.useCallback(() => onClick(node.id), [node.id, onClick]);
+    const clickSubHandler = React.useCallback(() => onSubClick(node.id), [node.id, onSubClick]);
+    return (
+      <div className={css.root} style={style}>
+        <div
+          className={classNames(
+            css.inner,
+            css[node.gender],
+            isRoot && css.isRoot,
+            isHover && css.isHover,
+          )}
+          onClick={clickHandler}
+        >
+          <div className={css.id}>{node.id}</div>
+          {node.hasSubTree && (
+            <div
+              className={classNames(css.sub, css[node.gender])}
+              onClick={clickSubHandler}
+            />
+          )}
+        </div>
+      </div>
+    );
+}
+
+export const NODE_WIDTH = 70;
+export const NODE_HEIGHT = 80;
+
+function getNodeStyle({ left, top }) {
+  return {
+    width: NODE_WIDTH,
+    height: NODE_HEIGHT,
+    transform: `translate(${left * (NODE_WIDTH / 2)}px, ${top * (NODE_HEIGHT / 2)}px)`,
+  };
+}
+
+function App() {
+  const nodes = require('./family.json')
+  const firstNodeId = React.useMemo(() => nodes[0].id, [nodes]);
+  const [rootId, setRootId] = React.useState(firstNodeId);
+
+  const [selectId, setSelectId] = React.useState("");
+  const [hoverId, setHoverId] = React.useState("");
+  return (
+    <div className={css.root}>
+      {nodes.length > 0 && (
+        <PinchZoomPan min={0.5} max={2.5} captureWheel className={css.wrapper}>
+          <ReactFamilyTree
+            nodes={nodes}
+            rootId={rootId}
+            width={NODE_WIDTH}
+            height={NODE_HEIGHT}
+            className={css.tree}
+            renderNode={node => (
+              <FamilyNode
+                key={node.id}
+                node={node}
+                isRoot={node.id === rootId}
+                isHover={node.id === hoverId}
+                onClick={setSelectId}
+                onSubClick={setRootId}
+                style={getNodeStyle(node)}
+              />
+            )}
+          />
+        </PinchZoomPan>
+      )}
+    </div>
+  );
+}
 
 
-ReactDOM.render(
-    <HashRouter>
-        <Switch>
-            { Families_List.map((family_var, key) => ( 
-                <Route path={ "/Tree/" + family_var} key={key} render={(props) =>(<Tree {...props} family={ require("./FamilyTree/FamilyInfo/" + family_var + ".json")} list={Families_List}/>)}/>
-            ))}
-            <Route path="/Map">
-                <Map />   
-            </Route>
-            <Route exact path="/">
-                <Home />
-            </Route>
-        </Switch>
-    </HashRouter>
-    , document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
